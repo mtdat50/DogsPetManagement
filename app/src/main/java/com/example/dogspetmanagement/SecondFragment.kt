10bd4e7064744b70d15f6154aa422af6db6de9bd
Hào Nhật <haonhatht@gmail.com>
2023-09-16 18:03:35 +0700

    Add update function, save function, change name to no editable

--- app/src/main/java/com/example/dogspetmanagement/SecondFragment.kt
+++ app/src/main/java/com/example/dogspetmanagement/SecondFragment.kt
@@ -5,14 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.dogspetmanagement.database.AppDatabase
+import com.example.dogspetmanagement.database.DogDao
 import com.example.dogspetmanagement.databinding.FragmentSecondBinding
+import com.example.dogspetmanagement.model.AppViewModel
+import kotlinx.coroutines.launch
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class SecondFragment : Fragment() {
-
+    private val sharedViewModel: AppViewModel by activityViewModels()
     private var _binding: FragmentSecondBinding? = null
+    private lateinit var dogDAO: DogDao
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,18 +31,45 @@ class SecondFragment : Fragment() {
     ): View {
 
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
-
-        binding.saveButton.setOnClickListener {
-
-        }
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
 
+        dogDAO = AppDatabase.getInstance(requireContext()).dogDao()
+
+        setDetail()
+
+        binding.saveButton.setOnClickListener {
+            val editedDogBreed = binding.editDogBreed.text
+            val editedDogDescription = binding.editDogDescription.text
+
+            val dogName = binding.dogName.text
+
+            lifecycleScope.launch {
+                dogDAO.updateDog(
+                    sharedViewModel.selectedDogInfo.imagePath,
+                    dogName.toString(),
+                    editedDogBreed.toString(),
+                    editedDogDescription.toString()
+                )
+            }
+        }
+    }
+
+    private fun setDetail() {
+        // set hint
+        binding.editDogBreed.hint = sharedViewModel.selectedDogInfo.breed
+        binding.editDogDescription.hint = sharedViewModel.selectedDogInfo.description
+
+        // set text
+        binding.dogName.text = sharedViewModel.selectedDogInfo.name
+        binding.editDogBreed.setText(sharedViewModel.selectedDogInfo.breed)
+        binding.editDogDescription.setText(sharedViewModel.selectedDogInfo.description)
+
+        // set image
     }
 
     override fun onDestroyView() {
