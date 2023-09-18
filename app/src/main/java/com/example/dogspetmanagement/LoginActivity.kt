edf8a2aecff9c34f952c43cbc0277e5c1272cc22
Mai Tấn Đạt <maitandat50@gmail.com>
2023-09-18 23:12:24 +0700

    Generate sample data on first run

--- app/src/main/java/com/example/dogspetmanagement/LoginActivity.kt
+++ app/src/main/java/com/example/dogspetmanagement/LoginActivity.kt
@@ -1,18 +1,29 @@
 package com.example.dogspetmanagement
 
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.dogspetmanagement.database.AppDatabase
+import com.example.dogspetmanagement.database.Dog
+import com.example.dogspetmanagement.database.DogDao
+import com.example.dogspetmanagement.database.User
 import com.example.dogspetmanagement.database.UserDao
 import com.example.dogspetmanagement.databinding.ActivityLoginBinding
 import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private lateinit var userDAO: UserDao
+    private lateinit var dogDAO: DogDao
     private var accountList = mutableListOf< Pair< String, String > >()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,12 +33,10 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         userDAO = AppDatabase.getInstance(this).userDao()
-        lifecycleScope.launch {
-//            inserted user:
-//            userDAO.insert(User("user1", "pass1"))
-//            userDAO.insert(User("user2", "pass2"))
-//            userDAO.insert(User("user3", "pass3"))
 
+//        generateSampleData()
+
+        lifecycleScope.launch {
             val queryResult = userDAO.getAll()
             for (user in queryResult)
                 accountList.add(Pair(user.username, user.password))
@@ -48,6 +57,60 @@ class LoginActivity : AppCompatActivity() {
             }
             else
                 binding.textView4.visibility = View.VISIBLE
+        }
+    }
+
+    private fun generateSampleData() {
+        // path to /data/data/yourapp/app_data/imageDir
+        var cw = ContextWrapper(this.applicationContext)
+        var directory = cw.getDir("imageDir", Context.MODE_PRIVATE)
+        var bitmapImage: Bitmap?
+        var imageSavePath: String
+
+        dogDAO = AppDatabase.getInstance(this).dogDao()
+
+        lifecycleScope.launch {
+            userDAO.insert(User("user1", "pass1"))
+            userDAO.insert(User("user2", "pass2"))
+            userDAO.insert(User("user3", "pass3"))
+
+
+            bitmapImage = BitmapFactory.decodeResource(this@LoginActivity.resources, R.drawable.n02085620_952)
+            imageSavePath = "$directory/1.jpg"
+            saveToInternalStorage(bitmapImage, imageSavePath)
+            dogDAO.insert(Dog(1, "dog1", imageSavePath, "Chihuahua", "description1"))
+
+            bitmapImage = BitmapFactory.decodeResource(this@LoginActivity.resources, R.drawable.n02085936_426)
+            imageSavePath = "$directory/2.jpg"
+            saveToInternalStorage(bitmapImage, imageSavePath)
+            dogDAO.insert(Dog(2, "dog2", imageSavePath, "Maltese", "description2"))
+
+            bitmapImage = BitmapFactory.decodeResource(this@LoginActivity.resources, R.drawable.n02088466_4825)
+            imageSavePath = "$directory/3.jpg"
+            saveToInternalStorage(bitmapImage, imageSavePath)
+            dogDAO.insert(Dog(3, "dog3", imageSavePath, "Bloodhound", "description3"))
+
+            bitmapImage = BitmapFactory.decodeResource(this@LoginActivity.resources, R.drawable.n02091831_749)
+            imageSavePath = "$directory/4.jpg"
+            saveToInternalStorage(bitmapImage, imageSavePath)
+            dogDAO.insert(Dog(4, "dog4", imageSavePath, "Saluki", "description4"))
+        }
+    }
+    private fun saveToInternalStorage(bitmapImage: Bitmap?, path: String) {
+        val myPath = File(path)
+        var fos: FileOutputStream? = null
+        try {
+            fos = FileOutputStream(myPath)
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage?.compress(Bitmap.CompressFormat.PNG, 100, fos)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            try {
+                fos!!.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
         }
     }
 }
